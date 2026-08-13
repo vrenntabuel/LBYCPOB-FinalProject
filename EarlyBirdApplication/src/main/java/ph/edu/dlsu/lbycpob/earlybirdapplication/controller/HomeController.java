e54c21a56d55c5c6b74ff43c85a6f380b2667b39
Arryan Paul D. Malirong <arryan_malirong@dlsu.edu.ph>
2026-08-13 18:52:43 +0800

    feat: connect home screen to dashboard

--- EarlyBirdApplication/src/main/java/ph/edu/dlsu/lbycpob/earlybirdapplication/controller/HomeController.java
+++ EarlyBirdApplication/src/main/java/ph/edu/dlsu/lbycpob/earlybirdapplication/controller/HomeController.java
@@ -1,10 +1,27 @@
 package ph.edu.dlsu.lbycpob.earlybirdapplication.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeController {
 
-    public void handleGetStarted(ActionEvent event) {
-        System.out.println("Get Started clicked!");
+    public void handleGetStarted(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource(
+                        "/ph/edu/dlsu/lbycpob/earlybirdapplication/dashboard-view.fxml"
+                )
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("EarlyBird - Dashboard");
+        stage.show();
     }
 }
