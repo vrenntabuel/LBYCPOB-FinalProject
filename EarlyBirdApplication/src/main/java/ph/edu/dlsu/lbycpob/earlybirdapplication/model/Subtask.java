2833efe5c9cade08f5fa25893399bfe9608cfd9b
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-08-17 21:40:28 +0800

    Added functionalities under Subtask.java

--- EarlyBirdApplication/src/main/java/ph/edu/dlsu/lbycpob/earlybirdapplication/model/Subtask.java
+++ EarlyBirdApplication/src/main/java/ph/edu/dlsu/lbycpob/earlybirdapplication/model/Subtask.java
@@ -1,4 +1,32 @@
 package ph.edu.dlsu.lbycpob.earlybirdapplication.model;
 
+import java.time.LocalDate;
+
 public class Subtask {
+
+    private String title;
+    private LocalDate dueDate;
+    private boolean completed;
+
+    public Subtask(String title, LocalDate dueDate) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
